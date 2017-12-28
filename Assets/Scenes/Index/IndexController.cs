4f8eccdb7393df9cefc2d974cf249f6399ea7ee9
Naldo Sandoval <h3rnaldo@gmail.com>
2017-12-28 20:17:32 -0300

    uploading files
    uploading files

---
+++ Assets/Scenes/Index/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IndexController : MonoBehaviour {
+
+	public Button yourButton;
+
+	void Start() {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick() {
+		Application.LoadLevel ("MetaGame");
+	}
+}
